3df6e02240ba6af049e0d1fb08b198117e43a387
Hesam <hrahmaniii20@gmail.com>
2023-06-11 20:34:10 +0330

    get data

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,75 @@
   <div class="h-100 w-full bg-my-color-primary-gray">
     <main-poster/>
     <middle-navbar/>
-    <trend-movies/>
+    <trend-movies :data="trendmovies"/>
   </div>
 </template>
 
 <script setup>
-import TrendMovies from './components/TrendMovies.vue';
+import trendMovies from './components/TrendMovies.vue';
 import mainPoster from './components/MainPoster.vue'
 import middleNavbar from './components/MiddleNavbar.vue'
+
+    // trend movies data
+    import movie1 from './assets/movie-1.png';
+    import movie2 from './assets/movie-2.png';
+    import movie3 from './assets/movie-3.png';
+    import movie4 from './assets/movie-4.png';
+    import movie5 from './assets/movie-5.png';
+    import movie6 from './assets/movie-6.png';
+    
+
+
+    const trendmovies = [
+        {
+            img: movie1,
+            imdb: 8.2,
+            genere: "Animation",
+            liked: "90%",
+            name: "Sonic",
+            id: 1
+        },
+        {
+            img: movie2,
+            imdb: 7.2,
+            genere: "Action",
+            liked: "85%",
+            name: "Morbius",
+            id: 2
+        },
+        {
+            img: movie3,
+            imdb: 7.6,
+            genere: "fantacy",
+            liked: "92%",
+            name: "Adam project",
+            id: 3
+        },
+        {
+            img: movie4,
+            imdb: 7.9,
+            genere: "Comedy",
+            liked: "92%",
+            name: "Free guy",
+            id: 4
+        },
+        {
+            img: movie5,
+            imdb: 7.7,
+            genere: "Action",
+            liked: "88%",
+            name: "Batman",
+            id: 5
+        },
+        {
+            img: movie6,
+            imdb: 7.8,
+            genere: "Action",
+            liked: "87%",
+            name: "Unchrted",
+            id: 6
+        },
+    ]
  
 </script>
 
--- src/components/TrendMovies.vue
+++ src/components/TrendMovies.vue
@@ -7,7 +7,7 @@
             </div>
             <div class=" w-full overflow-x-scroll whitespace-no-wrap mx-auto mt-5">
             <div class="flex flex-row space-x-3 mx-auto [&::-webkit-scrollbar]:hidden">
-                <div v-for="movie in trendMovies" :key="movie.id" class="text-white relative" aria-selected="true">
+                <div v-for="movie in data" :key="movie.id" class="text-white relative" aria-selected="true">
                     <div class="relative min-w-[150px] ">
                         <div class="absolute flex items-center justify-between left-2 right-2 top-1 z-20">
                             <img src="../assets/IMDB_Logo.png" class="w-10 h-4" alt="">
@@ -30,65 +30,7 @@
 </template>
 
 <script setup>
-    import movie1 from '../assets/movie-1.png';
-    import movie2 from '../assets/movie-2.png';
-    import movie3 from '../assets/movie-3.png';
-    import movie4 from '../assets/movie-4.png';
-    import movie5 from '../assets/movie-5.png';
-    import movie6 from '../assets/movie-6.png';
-    
-
-
-    const trendMovies = [
-        {
-            img: movie1,
-            imdb: 8.2,
-            genere: "Animation",
-            liked: "90%",
-            name: "Sonic",
-            id: 1
-        },
-        {
-            img: movie2,
-            imdb: 7.2,
-            genere: "Action",
-            liked: "85%",
-            name: "Morbius",
-            id: 2
-        },
-        {
-            img: movie3,
-            imdb: 7.6,
-            genere: "fantacy",
-            liked: "92%",
-            name: "Adam project",
-            id: 3
-        },
-        {
-            img: movie4,
-            imdb: 7.9,
-            genere: "Comedy",
-            liked: "92%",
-            name: "Free guy",
-            id: 4
-        },
-        {
-            img: movie5,
-            imdb: 7.7,
-            genere: "Action",
-            liked: "88%",
-            name: "Batman",
-            id: 5
-        },
-        {
-            img: movie6,
-            imdb: 7.8,
-            genere: "Action",
-            liked: "87%",
-            name: "Unchrted",
-            id: 6
-        },
-    ]
+    defineProps(['data'])
 </script>
 
 <style>
