7aaaa6f80b4aff977f89b1f7d08ebdcfb37db0e1
Hesam <hrahmaniii20@gmail.com>
2024-08-05 16:26:01 +0330

    feat: optimize search loading in people result section

---
+++ src/components/General/Pagination.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center mt-7">
+    <button class="text-white pr-2">-</button>
+    <button v-for="(item, index) in Array(5)" :key="index" class="border text-white px-2">{{ index + 1 }}</button>
+    <button class="text-white pl-2">+</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['totalResults'])
+</script>
+
+<style>
+
+</style>
--- src/components/SearchResult/PeopleResultSection.vue
+++ src/components/SearchResult/PeopleResultSection.vue
@@ -1,49 +1,22 @@
 <template>
-  <p class="text-white" v-if="loading">Loading...</p>
-  <p class="text-white" v-else-if="error">these is something wrong try again :(</p>
-  <div class="flex flex-wrap justify-between" v-else>
-    <people-rows :persons="showResult.slice(0, showCount)">
-      Casts
-    </people-rows>
-  </div>
-  <div class="text-center">
-    <button @click.prevent="showCount+=5" class="bg-yellow-400 mt-5 p-1 rounded-lg">Load more...</button>
-  </div>
+  <SearchLoader :endpoint="`${API_BASE_URL}/3/search/person`">
+    <template #loaded="{data, resultCount}" >
+      <people-rows :persons="data.slice(0, showCount)">
+        <template #title>Casts</template>
+        <template #pagination>
+          <Pagination :total-results="resultCount"/>
+        </template>
+      </people-rows>
+    </template>
+  </SearchLoader>
 </template>
 
 <script setup>
-import { useSearch } from '@/composables/useSearch.js'
 import PeopleRows from '@/components/General/PeopleRows.vue'
-import { API_BASE_URL, API_VERSION } from '@/components/ApiDetails/api-constant'
-import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-let { query } = route.query
-const showResult = ref([])
-const showPage = ref(1)
+import Pagination from '@/components/General/Pagination.vue'
+import SearchLoader from '@/components/General/SearchLoader.vue'
+import { API_BASE_URL } from '@/components/ApiDetails/api-constant'
+import { ref } from 'vue'
 const showCount = ref(5)
 
-const { data, error, loading, resultCount, doSearch } = useSearch()
-
-watch(showPage, () => {
-  
-})
-
-watch(route, (newRoute) => {
-    query = newRoute.query.query
-    showResult.value = []
-    showPage.value = 1
-    doSearch(`${API_BASE_URL}${API_VERSION}/search/person`, query, showPage.value)
-  },
-  { immediate: true }
-)
-
-watch(data, () => {
-  if (!loading.value) {
-    showResult.value.push(...data.value.results)
-  }
-})
 </script>
-
-<style></style>
