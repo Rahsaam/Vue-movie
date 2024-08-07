bf2e54fb63d143603e4b220cb98e4e3e0e4f9bb9
Hesam <hrahmaniii20@gmail.com>
2024-08-07 12:31:14 +0330

    feat: complete pagiantion component

--- src/components/General/Pagination.vue
+++ src/components/General/Pagination.vue
@@ -1,15 +1,87 @@
 <template>
-  <div class="flex justify-center mt-7">
-    <button class="text-white pr-2">-</button>
-    <button v-for="(item, index) in Array(5)" :key="index" class="border text-white px-2">{{ index + 1 }}</button>
-    <button class="text-white pl-2">+</button>
-  </div>
+  <ul class="text-white flex justify-center mt-8">
+    <li class="p-2">
+      <a href="#" @click.prevent="pageChanged(prevPage)" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li v-for="(i, n) in moviesPages" :class="activePage(n)" class="p-2" :key="n">
+      <a href="#" @click.prevent="pageChanged(n)">{{ n + 1 }}</a>
+    </li>
+    <li class="p-2">
+      <a href="#" @click.prevent="pageChanged(nextPage)" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script setup>
-defineProps(['totalResults'])
+import { computed, defineProps } from 'vue'
+import Util from '@/components/utils/pagination.js'
+
+const props = defineProps({
+  currentPage: {
+    type: Number,
+    required: true
+  },
+  totalPages: Number,
+  itemsPerPage: Number,
+  totalItems: Number,
+  url: String,
+  showPage: Number,
+  movies: Array,
+  visiblePages: {
+    type: Number,
+    default: 10
+  }
+})
+
+const emit = defineEmits('page-changed')
+
+// let currentPage = ref(0)
+
+const prevPage = computed(() => {
+  return props.currentPage > 1 ? props.currentPage - 1 : 0
+})
+
+const nextPage = computed(() => {
+  return props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages
+})
+
+const moviesPages = computed(() => {
+  return props.movies / props.itemsPerPage
+})
+
+const lastPage = computed(() => {
+  if (props.totalPages) {
+    return props.totalPages
+  } else {
+    return props.totalItems % props.itemsPerPage === 0
+      ? props.totalItems / props.itemsPerPage
+      : Math.floor(props.totalItems / props.itemsPerPage) + 1
+  }
+})
+
+const paginationRange = computed(() => {
+  let start =
+    props.currentPage - props.visiblePages / 2 <= 0
+      ? 1
+      : props.currentPage + props.visiblePages / 2 > lastPage.value
+      ? Util.lowerBound(lastPage.value - props.visiblePages + 1, 1)
+      : Math.ceil(props.currentPage - props.visiblePages / 2)
+  let range = []
+  for (let i = 0; i < props.visiblePages && i < lastPage.value; i++) {
+    range.push(start + i)
+  }
+  return range
+})
+
+const pageChanged = (pageNum) => {
+  emit('page-changed', pageNum)
+}
+
+const activePage = (pageNum) => {
+  return props.currentPage === pageNum ? 'active' : ''
+}
 </script>
-
-<style>
-
-</style>
--- src/components/SearchResult/MovieResultSection.vue
+++ src/components/SearchResult/MovieResultSection.vue
@@ -1,11 +1,33 @@
 <template>
-  hello
+  <SearchLoader :endpoint="`${API_BASE_URL}/3/search/movie`">
+    <template #loaded="{ data, resultCount, totalPages, showPage }">
+      <people-rows :persons="data.slice(startIndex, endIndex)">
+        <template #title>Casts</template>
+        <template #pagination>
+          <Pagination
+            :total-results="resultCount"
+            :items-per-page="itemsPerPage"
+            :total-pages="totalPages"
+            :current-Page="currentPage"
+            :show-page="showPage"
+            :movies="data.length"
+            @pageChanged="(pageNum) => currentPage = pageNum"
+          />
+        </template>
+      </people-rows>
+    </template>
+  </SearchLoader>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import Pagination from '@/components/General/Pagination.vue'
+import SearchLoader from '@/components/General/SearchLoader.vue'
+import { API_BASE_URL } from '@/components/ApiDetails/api-constant'
+import { computed, ref } from 'vue'
+const itemsPerPage = ref(5)
+const currentPage = ref(0)
+const startIndex = computed(() => currentPage.value * itemsPerPage.value)
+const endIndex = computed(() => (currentPage.value + 1) * itemsPerPage.value)
 </script>
 
 <style>
